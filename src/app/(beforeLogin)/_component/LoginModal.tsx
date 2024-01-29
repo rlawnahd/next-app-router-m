4907300a02f407593a773c756ba73f1b141424ac
rlawnahd <rlawnahd123@naver.com>
2024-01-29 17:22:31 +0900

    feat: 페러렐 라우트, 인터셉팅 라우트를 통한 로그인 회원가입  구현

--- src/app/(beforeLogin)/_component/LoginModal.tsx
+++ src/app/(beforeLogin)/_component/LoginModal.tsx
@@ -1,13 +1,17 @@
 'use client';
 import React, { useState } from 'react';
 import style from '@/app/(beforeLogin)/_component/login.module.css';
+import { useRouter } from 'next/navigation';
 
 export default function LoginModal() {
     const [id, setId] = useState();
     const [password, setPassword] = useState();
     const [message, setMessage] = useState();
+    const router = useRouter();
     const onSubmit = () => {};
-    const onClickClose = () => {};
+    const onClickClose = () => {
+        router.back();
+    };
 
     const onChangeId = () => {};
 
