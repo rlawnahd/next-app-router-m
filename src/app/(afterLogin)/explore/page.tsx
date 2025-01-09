684fdb38142424dc78d86084d6d7435bedde9991
kimjumong <rlawnahd123@naevr.com>
2025-01-09 12:01:14 +0900

    wip: search 작업 중

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,19 @@
+import style from './explore.module.css';
+import SearchForm from '../_component/SearchForm';
+import Trend from '../_component/Trend';
+
 export default function Page() {
-  return (
-    <main>
-      탐색
-    </main>
-  )
+    return (
+        <main className={style.main}>
+            <div className={style.formZone}>
+                <SearchForm />
+            </div>
+            <div className={style.trend}>
+                <h3>나를 위한 트렌드</h3>
+                <Trend />
+                <Trend />
+                <Trend />
+            </div>
+        </main>
+    );
 }
--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,7 +1,41 @@
-export default function Page() {
-  return (
-    <main>
-      검색
-    </main>
-  )
+import style from './search.module.css';
+import BackButton from '@/app/(afterLogin)/_component/BackButton';
+import SearchForm from '@/app/(afterLogin)/_component/SearchForm';
+import Post from '@/app/(afterLogin)/_component/Post';
+import Tab from './_component/Tab';
+
+type Props = {
+    searchParams: Promise<{ q: string; f?: string; pf?: string }>;
+};
+export default async function Search({ searchParams }: Props) {
+    const { q } = await searchParams;
+    return (
+        <main className={style.main}>
+            <div className={style.searchTop}>
+                <div className={style.searchZone}>
+                    <div className={style.buttonZone}>
+                        <BackButton />
+                    </div>
+                    <div className={style.formZone}>
+                        <SearchForm q={q} />
+                    </div>
+                </div>
+                <Tab />
+            </div>
+            <div className={style.list}>
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                {/*<SearchResult searchParams={searchParams} />*/}
+            </div>
+        </main>
+    );
 }
